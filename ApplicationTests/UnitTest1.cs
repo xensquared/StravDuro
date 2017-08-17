4e7425b0529b44d1457540f343335b3f74d6e41d
Brian Hadley <pebkak@gmail.com>
2017-08-16 19:28:40 -0700

    ## architecture layer commit # adds domain, application, infrastructure, persistence and common layer from template # several pocos added to domain to begin demonstrating concept

---
+++ ApplicationTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StravDuro.Common;
+
+namespace ApplicationTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Class1.paddedString("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sagittis dolor mauris, at elementum ligula tempor eget. In quis rhoncus nunc, at aliquet orci. Fusce at dolor sit amet felis suscipit tristique. Nam a imperdiet tellus. Nulla eu vestibulum urna. Vivamus tincidunt suscipit enim, nec ultrices nisi volutpat ac. Maecenas sit amet lacinia arcu, non dictum justo. Donec sed quam vel risus faucibus euismod. Suspendisse rhoncus rhoncus felis at fermentum. Donec lorem magna, ultricies a nunc sit amet, blandit fringilla nunc. In vestibulum velit ac felis rhoncus pellentesque. Mauris at tellus enim. Aliquam eleifend tempus dapibus. Pellentesque commodo, nisi sit amet hendrerit fringilla, ante odio porta lacus, ut elementum justo nulla et dolor.", 15);
+        }
+    }
+}
---
+++ Domain/Entities/Segment.cs
@@ -0,0 +1,9 @@
+﻿namespace StravDuro.Domain.Entities
+{
+    public class Segment
+    {
+        public string SegmentName { get; set; }
+        public Geo SegmentGeo { get; set; }
+        public string SegmentNativeId { get; set; }
+    }
+}
